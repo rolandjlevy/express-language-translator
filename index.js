c634e1db0b1e5ac03e3d77066a7ba815a2d25366
Roland Levy <rolandjlevy@gmail.com>
2020-05-24 22:30:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const translate = require('@vitalets/google-translate-api');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('pages/index', { msg:'', result:'', lang:'en', uri:req.originalUrl });
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about', { uri:req.originalUrl });
+});
+
+app.post('/translate', async (req, res) => {
+  const { msg, lang } = req.body;
+  const result = await translation(msg, lang);
+  res.render('pages/index', { msg, result, lang, uri:req.originalUrl });
+});
+
+function translation(msg, lang) {
+  return translate(msg, {to: lang})
+    .then(res => {
+      console.log('Originating language is', res.from.language.iso);
+      return res.text;
+    })
+    .catch(err => {
+      console.error(err);
+  });
+}
+
+app.listen(port, () => {
+  console.log('Listening on port ', port)
+});
---
+++ notes.md
@@ -0,0 +1,10 @@
+Resources
+===
++ [Node package](https://www.npmjs.com/package/@vitalets/google-translate-api)
++ [Video tutorial on google-translate-api](https://www.youtube.com/watch?v=zFwJUcuxK9Y)
+
+
+Ideas
+===
+Use ajax to post to /translate route
+Use express.Router()
